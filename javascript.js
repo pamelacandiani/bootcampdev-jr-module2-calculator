d0ebc4ea9adf36993939ef54ac9903619356b863
Pamela Candiani <pamela.candianipcs@gmail.com>
2024-01-19 21:41:08 -0300

    closes #4 - Implementar as 4 operacoes

--- javascript.js
+++ javascript.js
@@ -1,8 +1,15 @@
-function calc(){
+function calc(op){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
 
-    var output = num1 + num2;
+    var output = 0;
+
+    switch(op){
+        case '+' : output = num1 + num2; break;
+        case '-' : output = num1 - num2; break;
+        case '*' : output = num1 * num2; break;
+        case '/' : output = num1 / num2; break;
+    }
 
     document.getElementById("output").value = output;
 }
